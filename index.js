9303e0b466e9992cca24f93ba56e08903a8378d9
profapro <tchung@g.eulji.ac.kr>
2022-02-23 17:01:10 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,27 @@
+// index.js
+const express = require('express'); // express 임포트
+const app = express(); // app생성
+const port = 3000;
+
+app.get('/', function (req, res) {
+  res.send('hello world!!');
+});
+
+app.listen(port, () => console.log(`${port}포트입니다.`));
+
+// 몽구스 연결
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb://localhost:27017/test',
+    {
+      //useNewUrlPaser: true,
+      //useUnifiedTofology: true,
+       //useCreateIndex: true,
+       //useFindAndModify: false
+    }
+  )
+  .then(() => console.log('MongoDB conected'))
+  .catch((err) => {
+    console.log(err);
+  });
